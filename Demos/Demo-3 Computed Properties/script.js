0a8878aaa4eef512c05322e1d1cc256344723faa
santoshrajkumar <rajkumar.nits@gmail.com>
2020-09-14 22:55:00 +0530

    computedProperty

---
+++ Demos/Demo-3 Computed Properties/script.js
@@ -0,0 +1,25 @@
+new Vue({
+	
+    el: '#app',
+    data: {
+        counter: 0,
+        secondCounter: 0
+    },
+    
+    computed: {
+        output: function(){
+          console.log('Computed')
+          return this.counter > 5 ? 'Greater than 5' : 'Less Than 5';
+      }
+    },
+    
+    methods: {
+   
+           result: function(){
+          console.log('Method')
+          return this.counter > 5 ? 'Greater than 5' : 'Less Than 5';
+      }
+   
+    }
+    
+  })
