cf394f6800d03d9be0136e7887a140b0bff6677f
santoshrajkumar <rajkumar.nits@gmail.com>
2020-09-14 23:05:45 +0530

    watch

---
+++ Demos/Demo-4 Watch/readme.md
@@ -0,0 +1,3 @@
+## Watcher / Watch : looks for changes in properies
+
+- They may be alternative to computed property
--- Demos/Demo-4 Watch/script.js
+++ Demos/Demo-4 Watch/script.js
@@ -12,6 +12,16 @@ new Vue({
           return this.counter > 5 ? 'Greater than 5' : 'Less Than 5';
       }
     },
+
+    // Looks for change in value, if that happens, after 2 sec it'll make the counter 0
+    watch: {
+      counter: function(value){
+        var vm = this;
+        setTimeout(function() {
+          vm.counter = 0;
+        }, 2000)
+      }
+    },
     
     methods: {
    
