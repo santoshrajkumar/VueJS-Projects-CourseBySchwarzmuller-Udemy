d7fab817f5fd88a6d65a433550f64958c611912f
santoshrajkumar <rajkumar.nits@gmail.com>
2020-09-15 23:29:44 +0530

    vfors

---
+++ Conditionals and List Rendering/Demo-2 v-for List Rendering and more/script.js
@@ -0,0 +1,11 @@
+
+new Vue({
+	el: '#app',
+  data: {
+  	myPhones: ['Nokia 3220', 'Nokia 6260', 'Motorola L7', 'Nokia E63', 'Moto G 2', 'iPhone 6', 'iPhone X'],
+    persons: [
+    {name: 'Chow', age: 25, community: 'Tai Ahom'},
+    {name: 'Oscar', age: 29, community: 'Mexican'}
+    ]
+  }
+});
