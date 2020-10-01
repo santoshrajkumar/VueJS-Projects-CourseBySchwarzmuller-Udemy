eb998037e138709bab23699a5a75bb1b2fb75b85
santoshrajkumar <rajkumar.nits@gmail.com>
2020-10-01 23:59:07 +0530

    'rootComponentAppAndCreatingComponents'

---
+++ 7-Components in Vuejs/root_component/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <h1>Hello World ! {{ status }}</h1>
+  <hr>
+  <hello></hello>
+</div>
+  
+</template>
+
+<script>
+  export default {
+    data:function(){
+      return {
+        status: 'Happy'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ 7-Components in Vuejs/root_component/src/Home.vue
@@ -0,0 +1,29 @@
+<template>
+
+<div>
+    <p>Hello {{ name }}</p>
+    <button @click="changeName">Change</button>
+</div>
+    
+</template>
+
+
+<script>
+export default {
+    data: function(){
+        return {
+            name: 'Sntosh'
+        }
+    },
+    methods: {
+        changeName(){
+            this.name = 'NameChanged'
+        }
+    }
+}
+</script>
+
+
+<style>
+
+</style>
